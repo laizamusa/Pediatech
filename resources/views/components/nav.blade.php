fc84b23ccc8c8e2a9e843bc3b384e0e5efc88d0a
laizamusa <chabi050613@gmail.com>
2017-06-09 17:48:02 +0800

    nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,38 +1,3 @@
-{{--<div class="container bdb">--}}
-    {{--<nav class="navbar navbar-toggleable-md">--}}
-        {{--<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">--}}
-            {{--<span class="navbar-toggler-icon"></span>--}}
-        {{--</button>--}}
-        {{--<a class="navbar-brand" href="/"><img style="height: 80px; /*margin-left: 120px;*/ margin-left: 133px;"  src="/img/logo.png"></a>--}}
-        {{--<div class="collapse navbar-collapse row" id="navbarCollapse">--}}
-            {{--<ul class="navbar-nav col-4">--}}
-            {{--</ul>--}}
-            {{--<ul class="nav navbar-nav col-8 text-center">--}}
-                {{--<li class="nav-item active col-3">--}}
-                    {{--<a class="nav-link" href="/about">About Us</a>--}}
-                {{--</li>--}}
-                {{--<li class="nav-item col-3 dropdown">--}}
-                    {{--<a class="nav-link dropdown" data-toggle="dropdown"  href="/tech">Technologies</a>--}}
-                    {{--<ul class="dropdown-menu" data-hover="dropdown" >--}}
-                        {{--<li class="nav2"><a class="nav-link" href="/tech/#eztab">EZTab</a></li>--}}
-                        {{--<li class="nav2"><a class="nav-link" href="/tech/#suredose">SureDose</a></li>--}}
-                        {{--<li class="nav2"><a class="nav-link" href="/tech/#tasterite">TasteRite</a></li>--}}
-                        {{--<li class="nav2"><a class="nav-link" href="/tech/#toothfriendly">ToothFriendly</a></li>--}}
-                        {{--<li class="nav2"><a class="nav-link" href="/tech/#zincplus">ZincPlus</a></li>--}}
-                    {{--</ul>--}}
-                    {{--<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/tech">Technologies</a>--}}
-                {{--</li>--}}
-                {{--<li class="nav-item col-3">--}}
-                    {{--<a class="nav-link" href="/org">Our Organization</a>--}}
-                {{--</li>--}}
-                {{--<li class="nav-item col-3">--}}
-                    {{--<a class="nav-link" href="contact">Contact</a>--}}
-                {{--</li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-    {{--</nav>--}}
-{{--</div>--}}
-
 <div class="container bdb">
 
     <!-- Static navbar -->
@@ -46,24 +11,25 @@
             <ul class="navbar-nav col-4">
                 </ul>
             <ul class="nav navbar-nav col-lg-8 col-md-12 text-center">
-                <li class="nav-item active col-lg-3 col-md-12">
-                    <a class="nav-link" href="{{ url('/about') }}">About Us</a>
+                <li class="nav-item col-lg-3 col-md-12">
+                    <a class="nav-link {{ request()->is('about') ? 'activeMenus' : '' }}" href="{{ URL::to('/about') }}">About Us</a>
                 </li>
-                <li class="nav-item active col-lg-3 col-md-12 dropdown">
-                <a class="nav-link dropdown" data-toggle="dropdown"  href="/tech">Technologies</a>
+                <li class="nav-item col-lg-3 col-md-12 dropdown">
+                    {{--{{Request::fullUrl()}}--}}
+                <a class="nav-link dropdown {{ request()->is('tech') ? 'activeMenus' : '' }}" data-toggle="dropdown"  href="/tech">Technologies</a>
                     <ul class="dropdown-menu text-center" data-hover="dropdown" >
-                        <li class="nav2"><a class="nav-link" href="{{ url('/tech/#eztab') }}">EZTab</a></li>
-                        <li class="nav2"><a class="nav-link" href="{{ url('/tech/#suredose') }}">SureDose</a></li>
-                        <li class="nav2"><a class="nav-link" href="{{ url('/tech/#tasterite') }}">TasteRite</a></li>
-                        <li class="nav2"><a class="nav-link" href="{{ url('/tech/#toothfriendly') }}">ToothFriendly</a></li>
-                        <li class="nav2"><a class="nav-link" href="{{ url('/tech/#zincplus') }}">ZincPlus</a></li>
+                        <li class="nav2"><a class="nav-link {{ request()->fullUrl() == Request::root().'/tech/#eztab' ? 'activeMenus' : '' }}" href="/tech#eztab">EZTab</a></li>
+                        <li class="nav2"><a class="nav-link {{ request()->is('suredose') ? 'activeMenus' : '' }}" href="/tech#suredose">SureDose</a></li>
+                        <li class="nav2"><a class="nav-link {{ request()->is('tasterite') ? 'activeMenus' : '' }}" href="/tech#tasterite">TasteRite</a></li>
+                        <li class="nav2"><a class="nav-link {{ request()->is('toothfriendly') ? 'activeMenus' : '' }}" href="/tech#toothfriendly">ToothFriendly</a></li>
+                        <li class="nav2"><a class="nav-link {{ request()->is('zincplus') ? 'activeMenus' : '' }}" href="/tech#zincplus">ZincPlus</a></li>
                     </ul>
                 </li>
                 <li class="nav-item col-lg-4 col-md-12">
-                    <a class="nav-link" href="{{ url('/org') }}">Our Organization</a>
+                    <a class="nav-link {{ request()->is('org') ? 'activeMenus' : '' }}" href="{{ url('/org') }}">Our Organization</a>
                 </li>
                 <li class="nav-item col-lg-2 col-md-12">
-                    <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
+                    <a class="nav-link {{ request()->is('contact') ? 'activeMenus' : '' }}" href="{{ url('/contact') }}">Contact</a>
                 </li>
             </ul>
         </div>
